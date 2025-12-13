fdd89cb1a6189de635d22c86d414a59cce705ce9
abdullah <smabdulla958>
2025-12-13 22:45:55 +0500

    remove some isue

--- Components/footer.jsx
+++ Components/footer.jsx
@@ -82,8 +82,7 @@ export default function Footer() {
               <Link
                 href="https://www.instagram.com/humexa.adnest"
                 target="_blank"
-                className="w-10 h-10 flex items-center justify-center rounded-full bg-white/10 border border-white/20 hover:bg-gradient-to-tr hover:from-pink-50 hover:to-purple-600
-                hover:scale-110 transition-all duration-300"
+                className="w-10 h-10 flex items-center justify-center rounded-full bg-white/10 border border-white/20 hover:bg-gradient-to-tr hover:from-pink-5 hover:to-purple-600 hover:scale-110 transition-all duration-300"
               >
                 <FaInstagram className="text-white text-lg" />
               </Link>
