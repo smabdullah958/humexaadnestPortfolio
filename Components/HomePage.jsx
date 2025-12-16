ac946de556dfbfbd5f1e7f6d9173afede7598054
abdullah <smabdulla958@gmail.com>
2025-12-17 01:51:49 +0500

    remove the animation on a homage page

--- Components/HomePage.jsx
+++ Components/HomePage.jsx
@@ -1,15 +1,9 @@
 "use client"
 import Link from 'next/link'
- import Aos from 'aos';
- import { useEffect } from 'react'
  const HomePage=()=>{
-   useEffect(() => {
-     Aos.init({ duration: 150 });
-   }, []);
 
   return (
     <div
-    data-aos="fade-down"
     id="home">
       <div   className="relative min-h-[80vh] 2xl:min-h-[70vh] flex items-center justify-center overflow-hidden pt-10 md:pt-0 pb-5 md:pb-0 bg-[#ebf2f6] xl:mt-14">
       <div
