77ce5c458dc996efdadb65b75e8777c331e39d62
abdullah <smabdulla958@gmail.com>
2026-01-07 01:17:12 +0500

    add a top margin

--- Components/HomePage.jsx
+++ Components/HomePage.jsx
@@ -16,8 +16,8 @@ const HomePage=()=>{
             <span className='block text-[10px] sm:text-[12px] font-semibold mt-1 text-center'> Lead Generation & Digital Growth Experts</span>
           </div>
         </div>
-           <Image src="/LeadGeneration.png" width={300} height={100} className='sm:hidden' alt="hero image" loading='lazy' />
-          <p  className="text-sm lg:text-lg 2xl:text-xl mb-10 text-gray-800  md:ml-10 leading-relaxed  text-left md:w-[40vw] 2xl:w-[20vw] font-semibold">
+        <Image src="/LeadGeneration.png" width={300} height={100} className='sm:hidden  mt-9' alt="hero image" loading='lazy'/>
+          <p className="text-sm lg:text-lg 2xl:text-xl mb-10 text-gray-800 md:ml-10 leading-relaxed  text-left md:w-[40vw] 2xl:w-[20vw] font-semibold">
           Helping Home Services, POS SaaS, Law Firms & Property Management Companies Generate high-quality leads with proven Meta Ads Strategies. Achieve Low Cost-Per-Lead 
           <span className='font-extrabold'> ($10–$20) </span> and Maximize Revenue 
           <span className='font-extrabold'> ($50k+ Monthly Potential) </span> with 3+ Years of Targeted US Campaign Experience.
