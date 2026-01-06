a23dd90fabad69d56b9980ede17bd63fc8a90c55
abdullah <smabdulla958@gmail.com>
2026-01-07 00:14:02 +0500

    improve the layout of a hero secion

--- Components/HomePage.jsx
+++ Components/HomePage.jsx
@@ -7,19 +7,19 @@ import Link from 'next/link'
     id="home">
       <div   className="relative min-h-[80vh] 2xl:min-h-[70vh] flex items-center justify-center overflow-hidden pt-10 md:pt-0 pb-5 md:pb-0 bg-[#ebf2f6] xl:mt-14">
       <div
-        className="absolute inset-0  bg-contain bg-right bg-no-repeat h-[80vh] mt-24 sm:mr-10 rounded-2xl opacity-40 lg:opacity-50"
-         style={{
-           backgroundImage: `url('/LeadGeneration.png') `
-         }} loading="lazy"
+        className="absolute inset-0  bg-contain bg-right bg-no-repeat h-[80vh] mt-24 sm:mr-10 rounded-2xl opacity-40 lg:opacity-50 sm:bg-[url('/LeadGeneration.png')]"
       />
       
       <div className="relative z-10 container px-10 text-left text-gray-800 font-extrabold">
         <div className="max-w-xl sm:mt-10">
-          <h1 className="text-xl sm:text-3xl 2xl:text-4xl font-bold md:pl-10 my-3 mt-5 mb-20 sm:mb-10  ">
-              <span className='text-purple-700'>HUMEXA ADNEST</span> — Lead Generation & Digital Growth Experts
-          </h1>
+          <div className="text-xl sm:text-3xl 2xl:text-4xl font-bold md:pl-10  mt-5 mb-20 sm:mb-10 leading-tight">
+            <span className='text-center block'>  HUMEXA ADNEST </span>
+            <span className='block text-[10px] sm:text-[12px] font-semibold mt-1 text-center'> Lead Generation & Digital Growth Experts </span>
+          </div>
           <p  className="text-sm lg:text-lg 2xl:text-xl mb-10 text-gray-800  md:ml-10 leading-relaxed  text-left md:w-[40vw]  2xl:w-[20vw] font-semibold">
-          Helping Home Services, POS SaaS, Law Firms & Property Management Companies Generate High-Quality Leads with Proven Meta Ads Strategies. Achieve Low Cost-Per-Lead ($10–$20) and Maximize Revenue ($50k+ Monthly Potential) with 3+ Years of Targeted US Campaign Experience.
+          Helping Home Services, POS SaaS, Law Firms & Property Management Companies Generate high-quality leads with proven Meta Ads Strategies. Achieve Low Cost-Per-Lead 
+          <span className='font-extrabold'> ($10–$20) </span> and Maximize Revenue 
+          <span className='font-extrabold'> ($50k+ Monthly Potential) </span> with 3+ Years of Targeted US Campaign Experience.
 
             </p>
     </div>
