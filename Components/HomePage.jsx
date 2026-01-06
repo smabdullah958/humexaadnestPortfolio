7caf309f4fa42f57d3349909e1d93504aec899e6
abdullah <smabdulla958@gmail.com>
2026-01-07 00:37:00 +0500

    add a image ina hero section

--- Components/HomePage.jsx
+++ Components/HomePage.jsx
@@ -1,18 +1,17 @@
 "use client"
 import Link from 'next/link'
- const HomePage=()=>{
+import Image from 'next/image';
+const HomePage=()=>{
 
   return (
     <div
     id="home">
       <div   className="relative min-h-[80vh] 2xl:min-h-[70vh] flex items-center justify-center overflow-hidden pt-10 md:pt-0 pb-5 md:pb-0 bg-[#ebf2f6] xl:mt-14">
       <div
-        className="absolute inset-0  bg-contain bg-right bg-no-repeat h-[80vh] mt-24 sm:mr-10 rounded-2xl opacity-40 lg:opacity-50 sm:bg-[url('/LeadGeneration.png')]"
-      />
-      
+        className="absolute inset-0  bg-contain bg-right bg-no-repeat h-[80vh] mt-24 sm:mr-10 rounded-2xl opacity-40 lg:opacity-50 sm:bg-[url('/LeadGeneration.png')]"/>
       <div className="relative z-10 container px-10 text-left text-gray-800 font-extrabold">
         <div className="max-w-xl sm:mt-10">
-          <div className="text-xl sm:text-3xl 2xl:text-4xl font-bold md:pl-10  mt-5 mb-20 sm:mb-10 leading-tight">
+          <div className="text-xl sm:text-3xl 2xl:text-4xl font-bold md:pl-10  mt-5 mb-10 leading-tight">
             <span className='text-center block'>  HUMEXA ADNEST </span>
             <span className='block text-[10px] sm:text-[12px] font-semibold mt-1 text-center'> Lead Generation & Digital Growth Experts </span>
           </div>
@@ -20,21 +19,16 @@ import Link from 'next/link'
           Helping Home Services, POS SaaS, Law Firms & Property Management Companies Generate high-quality leads with proven Meta Ads Strategies. Achieve Low Cost-Per-Lead 
           <span className='font-extrabold'> ($10–$20) </span> and Maximize Revenue 
           <span className='font-extrabold'> ($50k+ Monthly Potential) </span> with 3+ Years of Targeted US Campaign Experience.
-
             </p>
     </div>
-    
+    <Image src="/LeadGeneration.png" width={300} height={100} className='sm:hidden' alt="hero image" loading='lazy' />
 <Link
   href="https://wa.me/923206841237?text=Hi%20I%20want%20to%20book%20a%2015-minute%20call."
   target="_blank"
-  className="md:hidden text-[10px] fixed bottom-5 right-5 px-5 py-3 bg-purple-600 text-white font-semibold rounded-full shadow-lg hover:bg-green-700 transition z-50"
->
+  className="md:hidden text-[10px] fixed bottom-5 right-5 px-5 py-3 bg-purple-600 text-white font-semibold rounded-full shadow-lg hover:bg-green-700 transition z-50">
   Get Free Consultation
 </Link>
-
-
     </div>
- 
     </div> 
     </div> 
 )};
