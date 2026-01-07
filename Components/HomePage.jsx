16e3f9252d3ce384f00578287afb7be92daf6cc5
abdullah <smabdulla958@gmail.com>
2026-01-07 16:57:56 +0500

    decrease the opacity ofa  image

--- Components/HomePage.jsx
+++ Components/HomePage.jsx
@@ -8,7 +8,7 @@ const HomePage=()=>{
     id="home">
       <div   className="relative min-h-[80vh] 2xl:min-h-[70vh] flex items-center justify-center overflow-hidden pt-10 md:pt-0 pb-5 md:pb-0 bg-[#ebf2f6] xl:mt-14">
       <div
-        className="absolute inset-0  bg-contain bg-right bg-no-repeat h-[80vh] mt-24 sm:mr-10 rounded-2xl opacity-40 lg:opacity-50 sm:bg-[url('/LeadGeneration.png')]"/>
+        className="absolute inset-0  bg-contain bg-right bg-no-repeat h-[80vh] mt-24 sm:mr-10 rounded-2xl opacity-20 sm:opacity-40  lg:opacity-50 bg-[url('/LeadGeneration.png')]"/>
       <div className="relative z-10 container px-10 text-left text-gray-800 font-extrabold">
         <div className="max-w-xl sm:mt-10">
           <div className="text-xl sm:text-3xl 2xl:text-4xl font-bold md:pl-10  mt-5 mb-5 sm:mb-10 leading-tight">
@@ -18,13 +18,13 @@ const HomePage=()=>{
         </div>
           <p className="text-sm lg:text-lg 2xl:text-xl mb-10 text-gray-800 md:ml-10 leading-relaxed  text-left md:w-[40vw] 2xl:w-[20vw] font-semibold">
           Helping Home Services, POS SaaS, Law Firms & Property Management Companies Generate high-quality leads with proven Meta Ads Strategies. Achieve Low Cost-Per-Lead 
-          <span className='font-extrabold'> ($10–$20) </span> and Maximize Revenue 
-          <span className='font-extrabold'> ($50k+ Monthly Potential) </span> with 3+ Years of Targeted US Campaign Experience.
+          <span className='font-extrabold text-black  '> ($10–$20) </span> and Maximize Revenue 
+          <span className='font-extrabold text-black  '> ($50k+ Monthly Potential) </span> with 3+ Years of Targeted US Campaign Experience.
             </p>
-     <div className='sm:hidden  mt-9 flex justify-center'>
+     {/* <div className='sm:hidden  mt-9 flex justify-center'>
         <Image src="/LeadGeneration.png" width={200} height={100}  alt="hero image" 
         loading='lazy' />
-        </div>
+        </div> */}
  
 <Link
   href="https://wa.me/923206841237?text=Hi%20I%20want%20to%20book%20a%2015-minute%20call."
