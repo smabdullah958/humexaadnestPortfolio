389c6237727db0dfbf47af9aba2b975a278b8794
Syed Muhammad Abdullah <159068479+smabdullah958@users.noreply.github.com>
2026-01-07 15:26:50 +0500

    Adjust image margin in HomePage component

--- Components/HomePage.jsx
+++ Components/HomePage.jsx
@@ -16,7 +16,7 @@ const HomePage=()=>{
             <span className='block text-[10px] sm:text-[12px] font-semibold mt-1 sm:text-center'> Lead Generation & Digital Growth Experts</span>
           </div>
         </div>
-        <Image src="/LeadGeneration.png" width={200} height={100} className='sm:hidden ml-10  mt-9' alt="hero image" loading='lazy' />
+        <Image src="/LeadGeneration.png" width={200} height={100} className='sm:hidden mx-3  mt-9' alt="hero image" loading='lazy' />
           <p className="text-sm lg:text-lg 2xl:text-xl mb-10 text-gray-800 md:ml-10 leading-relaxed  text-left md:w-[40vw] 2xl:w-[20vw] font-semibold">
           Helping Home Services, POS SaaS, Law Firms & Property Management Companies Generate high-quality leads with proven Meta Ads Strategies. Achieve Low Cost-Per-Lead 
           <span className='font-extrabold'> ($10–$20) </span> and Maximize Revenue 
